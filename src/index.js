8927f7434a82e96d1d8302a91c151261dac969c7
Vladyslav Kruchok <vladyslavw@gmail.com>
2022-12-24 20:34:08 +0200

    change basename on /

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import store, { persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+    <BrowserRouter basename="/">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
